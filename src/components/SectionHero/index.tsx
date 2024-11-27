2d50b52cbba7e3646446c8f6a4b196ef752d6e5b
Caio LK <caio.kozano@live.com>
2024-11-27 00:28:57 -0300

    feat: Adição de animação gsap nos sections

--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -1,17 +1,51 @@
+"use client";
 import Image from "next/image";
+import { useRef, useEffect } from 'react';
 import { GridContainer } from "../GridContainer";
 
 import IcontFile from '@/assets/icon-file.svg';
 import IconHand from '@/assets/icon-hand.svg';
 import IconHand02 from '@/assets/icon-hand-02.svg';
 import IconMockup from '@/assets/mockup.svg';
-import IconMainMockup from '@/assets/mockup-main.png'
+import gsap from 'gsap';
 
 export function SectionHero(){
+    const textHeroRef = useRef(null);
+    const mockupLeftRef = useRef(null);
+    const mockupRightRef = useRef(null);
+
+    useEffect(() => {
+        const textHero = textHeroRef.current;
+        const mLeft = mockupLeftRef.current;
+        const mRight = mockupRightRef.current;
+
+        gsap.fromTo(textHero, {
+            opacity: 0,
+            y: 20,
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            ease: 'power4.out'
+        })
+        gsap.fromTo(mLeft, {
+            rotate: 0
+        }, {
+            rotate: -10,
+            duration: 1
+        })
+        gsap.fromTo(mRight, {
+            rotate: 0
+        }, {
+            rotate: 10,
+            duration: 1
+        })
+    }, []);
+
     return (
         <section className="relative w-full h-section-hero bg-hero bg-no-repeat bg-top bg-green-primary border-t border-t-green-border pt-16">
             <GridContainer className="flex flex-col items-center">
-                <div className="w-full max-w-text-hero text-center">
+                <div className="w-full max-w-text-hero text-center opacity-0" ref={textHeroRef}>
                     <h3 className="text-xl text-green-actived mb-4">Novo curso focado em instagram</h3>
                     <h1 className="text-white text-7xl/normal font-semibold mb-4">Destrave as suas habilidades</h1>
                     <div className="flex items-center justify-center gap-10">
@@ -43,12 +77,14 @@ export function SectionHero(){
                     <Image 
                         src={IconMockup}
                         alt="Icon Mockup"
-                        className="relative -rotate-12 top-[1.1rem] left-[3.3rem]"
+                        className="relative top-[1.1rem] left-[3.3rem]"
+                        ref={mockupLeftRef}
                     />
                     <Image 
                         src={IconMockup}
                         alt="Icon Mockup"
-                        className="relative rotate-12 top-[1.1rem] right-[3.3rem]"
+                        className="relative top-[1.1rem] right-[3.3rem]"
+                        ref={mockupRightRef}
                     />
                 </div>
             </GridContainer>
